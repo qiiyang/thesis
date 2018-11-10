ece8b37bc42b07b296ba1cf9bce4ee698f62d6af
Q.I. Yang <yq.yangqi@gmail.com>
2018-11-09 17:24:59 -0800

    Mott figure.

--- figs_theories/mott.py
+++ figs_theories/mott.py
@@ -9,43 +9,27 @@ rcParams['text.latex.preamble'] = r'\usepackage{amssymb}'
 
 
 if __name__ == '__main__':
-    g = 0.5
-    y0 = 0.0
     y1 = 1.0
     x1 = 1.0
+    de0 = 0.3
+    de1 = 0.5
     
-    dx = 0.45
-    
-    n = 30
+    n = 120
     
     
     # Figure configs
-    plt.rc('font', family="Calibri")
+    #plt.rc('font', family="Calibri")
     plt.rc('font', size=12)
     plt.rc('ps', usedistiller="xpdf")
     
     paperwidth = 8.5
     fig = plt.figure(dpi=1200)
-    fig.set_size_inches([0.6 * paperwidth, 0.3 * paperwidth])
+    fig.set_size_inches([0.6 * paperwidth, 0.6 * paperwidth])
         
     sub = fig.add_subplot(111)
     
-    sub.set_xlabel(r"$E$")
-    #yl = sub.set_ylabel(r"$g(E)$")
-    #yl.set_rotation(0)
-
-    sub.xaxis.set_label_position("bottom")
-    sub.xaxis.set_label_coords(1, -0.05)
-    sub.yaxis.set_label_position("left")
-    sub.yaxis.set_label_coords(0.45, 1.0)
-
-    # Move left y-axis and bottim x-axis to centre, passing through (0,0)
-    sub.spines['left'].set_position(('data', 0.0))
-    sub.spines['bottom'].set_position(('data', 0.0))
-
-    # Eliminate upper and right axes
-    sub.spines['right'].set_color('none')
-    sub.spines['top'].set_color('none')
+    sub.set_xlabel(r"$x$", fontsize="large")
+    sub.set_ylabel(r"energy", fontsize="large")
 
     # Show ticks in the left and lower axes only    
     sub.tick_params(axis='both', which='both', direction="out", pad=5, bottom=False, top=False, left=False, right=False, labelleft=False, labelright=False, labeltop=False, labelbottom=False)
@@ -61,20 +45,23 @@ if __name__ == '__main__':
     #sub.plot(xs, ys, marker=None, linestyle='-', color="#2671b2")
     
     sub.set_xlim([-x1, x1])
-    sub.set_ylim([y0, y1])
-    sub.axhline(y=g, linestyle='-', color="#2671b2")
+    sub.set_ylim([-y1, y1])
     
-    sub.axvline(x=dx, linestyle='--', color="k")
-    sub.axvline(x=-dx, linestyle='--', color="k")
+    sub.axhline(y=0.0, linestyle='-', color="k")
+    sub.axhline(y=de0, linestyle='--', color="k")
+    sub.axhline(y=-de0, linestyle='--', color="k")
+    sub.axhline(y=de1, linestyle='--', color="#2671b2")
+    sub.axhline(y=-de1, linestyle='--', color="#2671b2")
     
     xs = (np.random.ranf((n,)) - 0.5) * 2.0 * x1
-    ys = np.ones(n) * 0.25
-    ind = xs < 0.0
-    sub.plot(xs[ind], ys[ind], marker='o', linestyle='none', color="#f19101", label="occupied")
-    ind = xs >= 0.0
-    sub.plot(xs[ind], ys[ind], marker='x', linestyle='none', color="#f19101", label="unoccupied")
+    ys = (np.random.ranf((n,)) - 0.5) * 2.0 * y1
+    ind = ys < 0.0
+    sub.plot(xs[ind], ys[ind], marker='x', linestyle='none', color="#f19101", label="filled states")
+    ind = ys >= 0.0
+    sub.plot(xs[ind], ys[ind], marker='o', linestyle='none', markerfacecolor="none", color="#f19101", label="empty states")
     
-    fig.legend()
+    l = fig.legend(loc="upper right", framealpha=1.0)
+    
     
     
     fig.tight_layout()
