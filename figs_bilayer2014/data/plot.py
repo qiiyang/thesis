a4065c3e90ae50d4bbae086e202538275e9bf0d0
Qi Yang <yq.yangqi@gmail.com>
2018-11-18 16:20:49 -0800

    quadratic plot: dynamic H

--- figs_bilayer2014/data/plot.py
+++ figs_bilayer2014/data/plot.py
@@ -9,11 +9,13 @@ import elflab.projects.ppms.analysis as ppmsana
 import elflab.analysis.transport as transport
 import elflab.analysis.temporal_drift as tdrift
 
-fn = r"20121020_02_RvH_2K_Ch1.10P.Al2O3.InterXX_Ch2.10Pa.EuS10.Al2O3.InterXX_Ch3.10Pb.EuS10.Al2O3.InterXX.dat"
+fn = r"20121020_04_RvH_10K_Ch1.10P.Al2O3.InterXX_Ch2.10Pa.EuS10.Al2O3.InterXX_Ch3.10Pb.EuS10.Al2O3.InterXX.dat"
 ch = 1
 
+H0 = 5.e2
 H1 = 5.e3
-H2 = 5.e3
+
+zero_H = 120.0
 
 if __name__ == '__main__':
 
@@ -34,30 +36,39 @@ if __name__ == '__main__':
     
     # calculate R(0)
     
-    d = d.sort("H")
-    R0 = d.interpolator("H", "R")(0.)
+    d = d.mask(np.abs(d["H"]) <= H1).sort("H")
+    #R0 = d.interpolator("H", "R")(0.)
+    R0 = np.mean(d["R"][np.abs(d["H"]) < zero_H])
     #print(R0)
     err2 = d.error2_interpolator("H", "R")(0.)
     
     
-    d = d.mask(np.abs(d["H"]) <= H2)
     d["mr"] = d["R"] / R0 - 1.0
     #d.errors["mr"] = np.sqrt(np.square(d.errors["R"] / R0) + np.square(d["R"])*err2 / R0**4)
     d.errors["mr"] = d.errors["R"] / R0
     
-    xs = np.power(d["H"][np.abs(d["H"]) <= H1], 2)
-    ys = d["mr"][np.abs(d["H"]) <= H1]
-    ps, rsqs, _, _ = np.linalg.lstsq(np.array([xs,]).T, ys, rcond=None)
-    p = ps[0]
-    rsq = rsqs[0]
-    std = np.std(xs)
-    n = xs.shape[0]
+    ratio = 2.0
     errsq = np.mean(d.errors["mr"]**2)
-    #print (errsq, rsq / (n-1))
-    dp = np.sqrt(rsq / (n-1) + errsq) / np.sqrt(n) / std
-    print(p, ",", dp)
+    mrsq = ratio * errsq + 1.0
     
-    plt.errorbar(d["H"], d["mr"], yerr= d.errors["mr"] * 2, marker="x", color="b", linestyle="none")
+    H2 = H1 * 3.0 / 2.0
+    d0 = d
+    while (mrsq > ratio * errsq) and (H2 > H0):
+        H2 = H2 *2.0 / 3.0
+        d = d.mask(np.abs(d["H"]) <= H2)
+        xs = np.power(d["H"], 2)
+        ys = d["mr"]
+        ps, rsqs, _, _ = np.linalg.lstsq(np.array([xs,]).T, ys, rcond=None)
+        n = xs.shape[0]
+        p = ps[0]
+        mrsq = rsqs[0] / (n-1)
+        std = np.std(xs)
+        errsq = np.mean(d.errors["mr"]**2)
+    
+    dp = np.sqrt(mrsq + errsq) / np.sqrt(n) / std
+    print(p, ",", dp, ",", H2, ",")
+    
+    plt.errorbar(d0["H"], d0["mr"], yerr= d0.errors["mr"] * 2, marker="x", color="b", linestyle="none")
     plt.plot(d["H"], d["H"]*d["H"]*p, marker=None, color="k", linestyle="-")
     
     plt.xlabel("$H$ / Oe")
